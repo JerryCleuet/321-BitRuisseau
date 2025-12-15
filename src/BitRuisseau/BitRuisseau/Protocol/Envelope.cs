cde2a6b4ea7ad0b6d6b1f2e6e248f2bf76d6a701
Jerry Cleuet <150446786+JerryCleuet@users.noreply.github.com>
2025-12-15 10:11:56 +0100

    feat(BitRuisseau): création du modèle d'enveloppe

---
+++ src/BitRuisseau/BitRuisseau/Protocol/Envelope.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitRuisseau.Protocol
+{
+    public class Envelope(string senderId, string receiverId, MessageType type, string message)
+    {
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+        public string SenderId { get; init; } = senderId;
+        public string? ReceiverId { get; init; } = receiverId;
+        public MessageType Type { get; init; } = type;
+        public string Message { get; set; } = message;
+    }
+}
